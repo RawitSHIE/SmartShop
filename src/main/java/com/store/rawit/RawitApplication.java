043ad822ff4e7285a5ce50c69a038c995dc5f76b
RawitSHIE <Rawitgun@gmail.com>
2019-09-03 14:31:58 +0700

    init project

---
+++ src/main/java/com/store/rawit/RawitApplication.java
@@ -0,0 +1,13 @@
+package com.store.rawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawitApplication.class, args);
+    }
+
+}
