beab7c471cc5b2114631ded3e436e0fc1d28864f
RawitSHIE <Rawitgun@gmail.com>
2019-09-04 19:20:50 +0700

    code refactor

--- src/main/java/com/store/rawit/StoreFacade.java
+++ src/main/java/com/store/rawit/StoreFacade.java
@@ -2,9 +2,7 @@ package com.store.rawit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @SpringBootApplication
